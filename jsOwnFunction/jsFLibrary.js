3c7623b072391a6372ac2265da152956b608287b
18230086651 <18230086651@163.com>
2018-05-06 23:29:55 +0800

    js库初步结构

--- jsOwnFunction/jsFLibrary.js
+++ jsOwnFunction/jsFLibrary.js
@@ -2,28 +2,28 @@
  * JavaScript functions library v1.1.0
  * http://www.zhenglinglu.cn/
  */
-(functions( w ) {
+(function( window ) {
 
     // 对外暴露的工厂函数
-    functions jQuery() {
-        return new jQuery.functions.init();
+    function jsFL() {
+        return new jsFL.functions.init();
     }
 
     // 给原型提供一个简写方式
-    jQuery.functions = jQuery.prototype = {
-        constrcutor: jQuery
+    jsFL.functions = jsFL.prototype = {
+        constrcutor: jsFL
     };
 
-    // init是jQuery中真正的构造函数 ==> 入口函数
-    var init = jQuery.functions.init = functions() {
+    // init是jsFL中真正的构造函数 ==> 入口函数
+    var init = jsFL.functions.init = functions() {
 
     };
 
-    // 替换构造函数的原型 为 jQuery工厂的原型
-    // 为了实现插件机制，让外界可以透过jQuery.functions扩充方法。
-    init.prototype = jQuery.functions;
+    // 替换构造函数的原型 为 jsFL工厂的原型
+    // 为了实现插件机制，让外界可以透过jsFL.functions扩充方法。
+    init.prototype = jsFL.functions;
 
     // 把工厂通过两个变量暴露出去
-    w.jQuery = w.$ = jQuery;
+    window.jsFL = window.$ = jsFL;
 
 }( window ));
