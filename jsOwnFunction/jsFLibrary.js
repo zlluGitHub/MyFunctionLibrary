0ff2aa3b6aa78830abf6cddd149615fede17395a
18230086651 <18230086651@163.com>
2018-05-07 23:55:27 +0800

    增加兼容绑定事件

--- jsOwnFunction/jsFLibrary.js
+++ jsOwnFunction/jsFLibrary.js
@@ -1,29 +1,63 @@
-/*!
- * JavaScript functions library v1.1.0
- * http://www.zhenglinglu.cn/
- */
-(function( window ) {
+     function jsFL(){
+         // 创建构造函数
+        function jsFunctionLibrary() {
+            this.name = "jsFunctionLibrary";
+        };
 
-    // 对外暴露的工厂函数
-    function jsFL() {
-        return new jsFL.functions.init();
-    }
+        // 给原型提供方法
+        jsFunctionLibrary.prototype = {
 
-    // 给原型提供一个简写方式
-    jsFL.functions = jsFL.prototype = {
-        constrcutor: jsFL
+            animate: function(ele, target, time) {
+                clearInterval(ele.timer);
+                ele.timer = setInterval(function() {
+                    var step = (target - ele.offsetTop) / 10;
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    ele.style.top = ele.offsetTop + step + "px";
+                    console.log(1);
+                    if (Math.abs(target - ele.offsetTop) < Math.abs(step)) {
+                        ele.style.top = target + "px";
+                        clearInterval(ele.timer);
+                    };
+                }, time);
+            },
+
+             // 兼容绑定事件
+            addEvent: function( ele, type, fn ){
+                if( ele.addEventListener ) {
+                    ele.addEventListener( type, fn);
+                }else {
+                    ele.attachEvent( 'on' + type, fn );
+                }
+            }
+            //scrollTop兼容封装
+            scroll: function() {
+                // 开始封装自己的scrollTop
+                if (window.pageYOffset != null) {
+                    // ie9+ 高版本浏览器
+                    // 因为 window.pageYOffset 默认的是  0  所以这里需要判断
+                    return {
+                        left: window.pageXOffset,
+                        top: window.pageYOffset
+                    };
+                } else if (document.compatMode === "CSS1Compat") {
+                    // 标准浏览器   来判断有没有声明DTD
+                    return {
+                        left: document.documentElement.scrollLeft,
+                        top: document.documentElement.scrollTop
+                    };
+                };
+                return {
+                    // 未声明 DTD
+                    left: document.body.scrollLeft,
+                    top: document.body.scrollTop
+                };
+            },
+
+            id: function(id){
+                return typeof id === "string" ? document.getElementById(id) : null;
+            }
+
+        };
+        return new jsFunctionLibrary();
     };
-
-    // init是jsFL中真正的构造函数 ==> 入口函数
-    var init = jsFL.functions.init = functions() {
-
-    };
-
-    // 替换构造函数的原型 为 jsFL工厂的原型
-    // 为了实现插件机制，让外界可以透过jsFL.functions扩充方法。
-    init.prototype = jsFL.functions;
-
-    // 把工厂通过两个变量暴露出去
-    window.jsFL = window.$ = jsFL;
-
-}( window ));
+    var jsFL = $= jsFL();
